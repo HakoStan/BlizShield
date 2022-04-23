8181b89ee6449936f2ecc6588680ed47b82f377d
efim <efim@ashkinazi.co.il>
2022-04-23 20:51:07 +0300

    Fixing and Making SMB Connect + FTP Connect More Generic

--- Clients/Plugins/Python/FtpScanner/main.py
+++ Clients/Plugins/Python/FtpScanner/main.py
@@ -1,6 +1,4 @@
 import logging
-import subprocess
-import datetime
 import ftplib
 
 
@@ -8,45 +6,40 @@ logger = logging.getLogger(__name__.split('.')[-1])
 
 
 class FtpChecker:
-    def __init__(self, ip: str, user='anonymous', password='') -> None:
+    def __init__(self, ip: str, user: str = "anonymous", password: str = ""):
         self.__user = user
         self.__password = password
         self.__ip = ip
 
     def execute(self) -> list[dict]:
-        server = ""
+        server = None
         ftp_info = {}
         data = []
-        connect = True
-        scan_timestamp = datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat()
+        connect = False
 
         # Init ftp connection
         try:
             server = ftplib.FTP()
             server.connect(self.__ip, 21)
-        except Exception as ex:
-            connect = False
-            logger.error(f"Exception connecting to {self.__ip}")
-            logger.error(f"{str(ex)}")
-
-        try:
             server.login(self.__user, self.__password)
-
+            server.quit()
+            connect = True
         except Exception as ex:
-            connect = False
             logger.error(f"Exception login {self.__ip}")
             logger.error(f"{str(ex)}")
 
         ftp_info = {"ip": self.__ip, "protocol": "FTP",
-                    "username": self.__user, "password": self.__password, "status": connect,
-                    "@timestamp": scan_timestamp}
+                    "username": self.__user, "password": self.__password, "status": connect}
         data.append(ftp_info)
         return data
 
 
 def run(config: dict) -> str:
     logger.info("FtpScan Starting")
-    ftp_check = FtpChecker(config["ip"], config["username"], config["password"])
+    if "username" in config.keys() and "password" in config.keys():
+        ftp_check = FtpChecker(config["ip"], config["username"], config["password"])
+    else:
+        ftp_check = FtpChecker(config["ip"])
     return ftp_check.execute()
 
 
--- Clients/Plugins/Python/SmbScanner/main.py
+++ Clients/Plugins/Python/SmbScanner/main.py
@@ -1,6 +1,4 @@
 import logging
-import subprocess
-import datetime
 import socket
 from smb import SMBConnection
 
@@ -8,50 +6,55 @@ logger = logging.getLogger(__name__.split('.')[-1])
 
 
 class SmbChecker:
-    def __init__(self, user: str, password: str, client: str, ip: str, domain: str,
+    def __init__(self, user: str, password: str,  ip: str, domain: str = "",
                  timeout=30) -> None:
         self.__user = user
         self.__password = password
-        self.__client = client
         self.__ip = ip
         self.__domain = domain
         self.__timeout = timeout
 
     def execute(self) -> list[dict]:
-        output = ""
-        host = ""
-        shares = []
-        data = []
-        scan_timestamp = datetime.datetime.now(datetime.timezone.utc).astimezone().isoformat()
+        host = None
+        info = {"ip": self.__ip, "host": "", "connected": False, "shares": []}
 
         # Gets computer name
         try:
-            host = socket.gethostbyaddr(self.__ip)
+            host = socket.gethostbyaddr(self.__ip)[0]
         except Exception as ex:
             logger.error(f"Exception translating {self.__ip} to hostname")
             logger.error(f"{str(ex)}")
 
+        # If couldn't get computer name, return
+        if not host:
+            return [info]
+        info["host"] = host
+
         try:
-            con = SMBConnection.SMBConnection(self.__user, self.__password, self.__client, host,
-                             self.__domain, use_ntlm_v2=True)
-            con.connect(host, 139)
+            con = SMBConnection.SMBConnection(self.__user, self.__password, socket.gethostname(), host, self.__domain, use_ntlm_v2=True)
+            if not con.connect(self.__ip, 139):
+                return [info]
         except Exception as ex:
             logger.error(f"Exception connecting {self.__ip}")
             logger.error(f"{str(ex)}")
+            return [info]
+        info["connected"] = True
 
         shares = con.listShares(timeout=self.__timeout)
         for share in shares:
-            share_info = {"ip": self.__ip, "host": host, "protocol": "SMB",
-                             "share_name": shares.name, "@timestamp": scan_timestamp}
-            data.append(share_info)
+            info["shares"].append({"name": share.name, "comments": share.comments})
 
-        return data
+        con.close()
+        return [info]
 
-    def run(config: dict) -> str:
-        logger.info("SmbScan Starting")
-        smb_checker = SmbChecker(config["user"], config["password"], config["client"],
-                               config["ip"], config["domain"])
-        return smb_checker.execute()
+
+def run(config: dict) -> str:
+    logger.info("SmbScan Starting")
+    if "domain" not in config.keys():
+        smb_checker = SmbChecker(config["username"], config["password"], config["ip"])
+    else:
+        smb_checker = SmbChecker(config["username"], config["password"], config["ip"], config["domain"])
+    return smb_checker.execute()
 
 
 
